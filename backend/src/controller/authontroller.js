dcdc3eea76b8f9b06d738df29b1656627dd1655f
yuvraj-6353 <yuviraj1107@gmail.com>
2025-12-29 15:28:43 +0530

    signup section refactor

--- backend/src/controller/authontroller.js
+++ backend/src/controller/authontroller.js
@@ -37,8 +37,8 @@ export const signup = async (req, res) => {
     });
 
     if (newUser) {
-      generateToken(newUser._id, res);
-      await newUser.save();
+      const savedUser = await newUser.save();
+      generateToken(savedUser._id, res);
       res.status(201).json({ message: "User created successfully" });
     } else {
       return res.status(500).json({ message: "Server error" });
