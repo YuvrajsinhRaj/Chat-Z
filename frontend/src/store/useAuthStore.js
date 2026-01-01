25d55076e57076fecc1a7475e69395bb6918aad3
yuvraj-6353 <yuviraj1107@gmail.com>
2026-01-01 23:03:41 +0530

    frontend-signupPage

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,10 +1,46 @@
-import {create} from 'zustard';
+import { create } from "zustand";
+import { axiosInstance } from "../config/axios.js";
+import toast from "react-hot-toast";
 
-export const useAuthStore = create((set,get)=>{
-    authUser:{name:"john"}
-    isLoggedIn:false,
-    isLoading:true,
-    login:()=>{
-        set({isLoggedIn:true,isLoadng:false}),
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isCheckingAuth: true,
+  isSigningUp: false,
+  isLoggingIn: false,
+
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+
+      set({
+        authUser: res.data,
+      });
+    } catch (error) {
+      console.error("Error in Auth Check:", error);
+
+      set({
+        authUser: null,
+      });
+    } finally {
+      set({
+        isCheckingAuth: false,
+      });
     }
-})
+  },
+
+  signup: async (data) => {
+    set({ isSigningUp: true });
+
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res.data });
+
+      toast.success("Account Created Successfully!");
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
+}));
