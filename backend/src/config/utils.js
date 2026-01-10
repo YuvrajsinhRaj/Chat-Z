045967097b62c0deda502ab074b932799d310be9
Yuvrajsinh Raj <yuviraj1107@gmail.com>
2026-01-10 22:52:15 +0530

    Refactor token generation and cookie settings

--- backend/src/config/utils.js
+++ backend/src/config/utils.js
@@ -1,15 +1,22 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
-  const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
-  });
+  const token = jwt.sign(
+    { id: userId },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
+
+  const isProduction = process.env.NODE_ENV === "production";
+
   res.cookie("token", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-    httpOnly: true, // accessible only by web server through HTTP and not by client-side JS &prevent XSS attacks:cross-site scripting
-    sameSite: process.env.NODE_ENV === "production"  ? "none" : "lax"
-    // sameSite: "strict", // CSRF protection
-    secure: process.env.NODE_ENV === "development" ? false : true,
+    httpOnly: true,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+
+    // 🔐 REQUIRED FOR CROSS-DOMAIN AUTH
+    sameSite: isProduction ? "none" : "lax",
+    secure: isProduction,
   });
+
   return token;
 };
