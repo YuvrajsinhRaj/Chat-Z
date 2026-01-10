82b520f3fa03092dedd41d38f9fb89096ae8fb8e
Yuvrajsinh Raj <yuviraj1107@gmail.com>
2026-01-10 16:25:56 +0530

    Update axios base URL for production environment

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3000/api"
-      : "/api",
+      : "https://chat-z-backend.onrender.com/api",
   withCredentials: true,
 });
