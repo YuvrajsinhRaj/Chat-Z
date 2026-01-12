9ff2aea5fe54c0b8ea2c71b6638c425afdfdb1f4
Yuvrajsinh Raj <yuviraj1107@gmail.com>
2026-01-12 16:36:20 +0530

    Rename MONGODB_URL to MONGO_URI in env.js

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -2,13 +2,13 @@ import "dotenv/config";
 
 export const ENV = {
   PORT: process.env.PORT,
-  MONGODB_URL: process.env.MONGODB_URL,
+  MONGO_URI: process.env.MONGO_URI,
   JWT_SECRET: process.env.JWT_SECRET,
+  NODE_ENV: process.env.NODE_ENV,
+  CLIENT_URL: process.env.CLIENT_URL,
   CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
   CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
   CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
-  CLIENT_URL: process.env.CLIENT_URL,
-  NODE_ENV: process.env.NODE_ENV,
   ARCJET_KEY: process.env.ARCJET_KEY,
   ARCJET_ENV: process.env.ARCJET_ENV,
 };
